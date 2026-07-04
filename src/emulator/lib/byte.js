8892d3f4293b60987e7774c0078bf9e9deace68e
zionhigt <larcher501@gmail.com>
2026-07-04 22:00:02 +0200

    [WIP] test pass

---
+++ src/emulator/lib/byte.js
@@ -0,0 +1,51 @@
+/**
+ * 
+ * @param {*} byte x8 bits 
+ * @param {*} position 0 = plus à droite et 7 plus à gauche
+ */
+export function getBit(byte, position) {
+    const mask = 1 << position;
+    return byte & mask;
+}
+
+export function getFlag(byte, position) {
+    return !!getBit(byte, position)
+}
+
+export function revertBits(byte) {
+    for (let i = 0; i < 8; i++) {
+        byte = setBit(byte, i, +!getFlag(byte, i));
+    }
+    return byte;
+}
+export function setBit(byte, position, value) {
+    value = +(!!value)
+    if (value) {
+        return byte | (value << position);
+    } else {
+        return byte & (0xff - (2 ** position));
+    }
+}
+
+export function buildU16(high, low) {
+    high <<= 8;
+    return high | low;
+}
+/**
+ * 
+ * @param {*} byte 
+ * return two 8bits byte
+ */
+export function U16to2U8(byte) {
+    const high = (byte >> 8);
+    const low = byte % 256;
+    return { high, low };
+}
+
+export default {
+    getBit,
+    getFlag,
+    setBit,
+    buildU16,
+    U16to2U8,
+};
