dc13f6f7cd4985fb2bf68fcffdf886c9e4af8c23
zionhigt <larcher501@gmail.com>
2026-07-05 10:31:39 +0200

    [FAKE]

--- src/store/slices/emulatorSlice.js
+++ src/store/slices/emulatorSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import CPU from '../../emulator/core/cpu/CPU';
 
 const initialState = {
   status: 'idle',
@@ -11,6 +12,11 @@ const emulatorSlice = createSlice({
   reducers: {
     setStatus(state, action) {
       state.status = action.payload;
+      // if (state.status === "running") {
+      //   const cpu = new CPU();
+      //   cpu.registers.AF.setValue(0xAA00)
+      //   cpu.registers.F.z = 1;
+      // }
     },
     loadRom(state, action) {
       state.rom = action.payload;
