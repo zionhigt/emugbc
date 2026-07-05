e032349938e3a92717aa3457e9976b5b1536f0cd
zionhigt <larcher501@gmail.com>
2026-07-05 10:30:43 +0200

    [IMPL] Minimal CPUMemory

---
+++ src/emulator/core/cpu/CPUMemory.js
@@ -0,0 +1,17 @@
+export default function() {
+    class Memory {
+        constructor() {
+            this.ram = new Uint8Array(0x10000)
+        }
+
+        write(address, value) {
+            this.ram[address] = value;
+        }
+
+        read(address) {
+            return this.ram[address];
+        }
+    }
+
+    return Memory;
+}
