78d4d71838c5b70e89b86d05e757c1ea0b6b7b83
zionhigt <larcher501@gmail.com>
2026-07-11 14:08:10 +0200

    [IMPL] Minimal MBC1

--- src/emulator/core/cartridge/mbc/MBC1.js
+++ src/emulator/core/cartridge/mbc/MBC1.js
@@ -1,13 +1,33 @@
+import { Register } from "../../../lib/register.js"
+
+class RBNRegister extends Register(8) {
+    setValue(value) {
+        value &= 0x1f;
+        return super.setValue(value);
+    }
+}
+
 export default class MBC1 {
     constructor(rom) {
         this.rom = rom;
+        this.RBN = new RBNRegister();
+        this.RBN.setValue(1);
     }
 
-    read() {
-        return this.rom.read.apply(this.rom, arguments);
+    read(addr) {
+        if (addr >= 0x4000) {
+            addr = this.RBN.getValue() * 0x4000 + (addr - 0x4000);
+        }
+        return this.rom.read(addr);
     }
 
     write(addr, value) {
-        // return this.rom.write();
+        if (addr >= 0x2000 && addr <= 0x3FFF) {
+            this.RBN.setValue(value);
+            value = this.RBN.getValue();
+            if (value === 0) {
+                this.RBN.setValue(1);
+            }
+        }
     }
 }
