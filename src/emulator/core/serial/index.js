08992de87b0e0a28df7eddb9b43ce95140c22732
zionhigt <larcher501@gmail.com>
2026-07-12 10:10:05 +0200

    [INIT] Minimal serial bus

---
+++ src/emulator/core/serial/index.js
@@ -0,0 +1,12 @@
+export default function(logger) {
+    class SerialBus {
+        constructor() {}
+
+        read () {};
+        write () {};
+
+        echo(value) {
+            logger.log(value);
+        }
+    }
+}
