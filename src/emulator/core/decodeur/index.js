013ffbb90b7cc6a204a420ea48c024fb94c8db1f
zionhigt <larcher501@gmail.com>
2026-07-12 06:53:11 +0200

    [IMPL] Decodeur

---
+++ src/emulator/core/decodeur/index.js
@@ -0,0 +1,53 @@
+import byte from "../../lib/byte.js"
+import opecodes from "../cpu/opcodes.js"
+
+export default function(cpu, instructions) {
+    class Decoder {
+        constructor() {
+            this.memory = cpu.memory;
+        }
+        fetch() {
+            const value = this.memory.read(cpu.registers.PC.getValue());
+            cpu.registers.PC.increment();
+            return value;
+        }
+        step() {
+            let main = opecodes.main;
+            let currentbyte = this.fetch();
+            if (currentbyte === 0xCB) {
+                main = opecodes.cb;
+                currentbyte = this.fetch();
+            }
+            if (!main[currentbyte]) throw new Error("Unknown opecodes");
+            const [id, ..._args] = main[currentbyte];
+            const args = [];
+            for (let arg of _args) {
+                if (["n8", "a8", "e8"].includes(arg)) {
+                    args.push(this.fetch());
+                } else if(arg === "n16") {
+                    const low = this.fetch();
+                    const high = this.fetch();
+                    args.push(
+                        byte.buildU16(
+                            high,
+                            low,
+                        )
+                    )
+                } else if (typeof arg === "string" && arg.startsWith("cc:")) {
+                    args.push(arg.slice(3, arg.length));
+                } else if(!Number.isNaN(Number.parseInt(arg))) {
+                    args.push(arg);
+                } else {
+                    args.push(cpu.registers[arg]);
+                }
+            }
+
+            instructions[id].run(cpu, ...args);
+            const cycles = cpu.cycles + instructions[id].cycle;
+            cpu.resetCycles();
+            return cycles;
+        }
+    }
+
+    return Decoder;
+}
