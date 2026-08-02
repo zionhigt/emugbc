130ffab7ccfc38e84204a40e45ed956090e9c9da
zionhigt <larcher501@gmail.com>
2026-08-02 15:18:44 +0200

    [IMPL] Bootstrap chan4

---
+++ src/emulator/core/apu/channel4.js
@@ -0,0 +1,12 @@
+import { Register } from "../../lib/register";
+import byte from "../../lib/byte";
+import { Channel } from "./channel";
+
+export default function(apu) {
+    function ChanFactory(start, Parent) {
+        class Chan extends Parent { }
+    
+        return new Chan(apu, start);
+    }
+    return Channel(0xFF1F, ChanFactory);
+}
--- src/emulator/core/apu/index.js
+++ src/emulator/core/apu/index.js
@@ -4,6 +4,7 @@ import { Register } from "../../lib/register";
 import channel1 from "./channel1";
 import channel2 from "./channel2";
 import channel3 from "./channel3";
+import channel4 from "./channel4";
 import Nr52 from "./nr52";
 
 
@@ -14,9 +15,11 @@ export default function(machine) {
             this.channel1 = channel1(this);
             this.channel2 = channel2(this);
             this.channel3 = channel3(this);
+            this.channel4 = channel4(this);
             this._isPowered = true;
             this.nr52 = Nr52(this);
-            this.ff15 = new (Register(8));
+            this.nr50 = new (Register(8));
+            this.nr51 = new (Register(8));
 
         }
 
@@ -33,10 +36,13 @@ export default function(machine) {
 
         get registersMapping() {
             return {
+                0xFF24: this.nr50,
+                0xFF25: this.nr51,
                 0xFF26: this.nr52,
                 ...this.channel1.registers,
                 ...this.channel2.registers,
                 ...this.channel3.registers,
+                ...this.channel4.registers,
             }
         }
         get maskRegistersMapping() {
@@ -54,6 +60,8 @@ export default function(machine) {
                 0xFF1D: 0xFF,
                 0xFF1E: 0xBF,
                 0xFF1F: 0xFF,
+                0xFF20: 0xFF,
+                0xFF23: 0xBF,
             }
         }
 
